a5b43b7405a364578ccf4e029c303aefbf86e05e
akshaygiridhar7 <akshaygiridhar7@gmail.com>
2019-02-12 15:17:22 +0530

    Test initial 2

--- app/src/main/java/com/everystepcounts/akshay/everystepcounts2/MainActivity.java
+++ app/src/main/java/com/everystepcounts/akshay/everystepcounts2/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.Objects;
 
 
@@ -14,15 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText Username, Password;
     private Button Login;
+    private Button register;
 
+    private FirebaseAuth mAuth=FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Username = (EditText) findViewById(R.id.etName);
         Password = (EditText) findViewById(R.id.etPassword);
         Login = (Button) findViewById(R.id.btnLogin);
+        register = (Button) findViewById(R.id.btnregister);
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
                 {
                     Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register_Page.class);
+                startActivity(intent);
+
             }
         });
     }
---
+++ app/src/main/java/com/everystepcounts/akshay/everystepcounts2/Register_Page.java
@@ -0,0 +1,68 @@
+package com.everystepcounts.akshay.everystepcounts2;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Register_Page extends AppCompatActivity {
+
+    private EditText Name, Email, Password;
+    private Button Register;
+    private RadioGroup rg;
+    private RadioButton rMale, rFemale;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    public void onClick(View v) {
+        mAuth.createUserWithEmailAndPassword(Email.getText().toString(), Password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.i("Register", "createUserWithEmail:success");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Not Registered", "createUserWithEmail:failure", task.getException());
+
+                        }
+
+                        // ...
+                    }
+                });
+
+
+
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register__page);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        Name = (EditText) findViewById(R.id.etName);
+        Email = (EditText) findViewById(R.id.etEmail);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Register = (Button) findViewById(R.id.btnRegister);
+        rMale = (RadioButton) findViewById(R.id.rbtnMale);
+        rFemale = (RadioButton) findViewById(R.id.rbtnFemale);
+
+    }
+}
+
+
